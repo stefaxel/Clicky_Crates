b1b5d2351943b6f735f198510aeccc1b41538811
stefaxel <stefan.axelson@sky.com>
2022-09-26 16:30:08 +0100

    Adding TMP to scene. Adding a method to increase the score

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -11,10 +11,13 @@ public class Target : MonoBehaviour
     private float xRange = 4f;
     private float ySpawnPos = -2f;
 
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(),RandomTorque(), RandomTorque(), ForceMode.Impulse);
@@ -46,6 +49,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(5);
     }
 
     private void OnTriggerEnter(Collider other)
