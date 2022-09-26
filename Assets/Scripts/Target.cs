1452ed00438dd2ab186c27efaf61b0f2a9f18a4e
stefaxel <stefan.axelson@sky.com>
2022-09-26 16:07:08 +0100

    Using mouse click to destroy game objects, using a trigger to destory game objects. Using IENumerator for spawning objects

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -9,7 +9,7 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16f;
     private float maxTorque = 10f;
     private float xRange = 4f;
-    private float ySpawnPos = -6f;
+    private float ySpawnPos = -2f;
 
     // Start is called before the first frame update
     void Start()
@@ -41,5 +41,15 @@ public class Target : MonoBehaviour
     Vector3 RandomSpawnPos()
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
