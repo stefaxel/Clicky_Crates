7505c69bddfe00dbb34eb78381fd8dc9dd08a718
stefaxel <stefan.axelson@sky.com>
2022-09-27 14:07:32 +0100

    Adding difficulty buttons to the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,16 +12,14 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI gameOver;
     public Button restartButton;
     public bool isGameActive;
+    public GameObject titleScreen;
     private int score;
     private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        score = 0;
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -57,5 +55,16 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
     }
 }
